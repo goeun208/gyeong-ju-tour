ddef10fea900417cdda8a5de0b5e7688c6f236ba
goeun208 <qop341@gmail.com>
2023-06-20 17:55:35 +0900

    반응형 사이즈 조절

--- src/components/hanokIntroduce.tsx
+++ src/components/hanokIntroduce.tsx
@@ -1,6 +1,6 @@
 const HanokIntroduce = ({element3}: any) => {
     return (
-        <div className="bg-[#C2CACD]" ref={element3}>
+        <div className="bg-[#081113]" ref={element3} style={{ background: 'linear-gradient(180deg, rgba(8,17,19,1) 0%, rgba(95,102,104,1) 49%, rgba(143,151,153,1) 100%)' }}>
             <div className="w-[22.5rem] md:w-[45rem] xl:w-[90rem] h-[60rem] mx-auto">
                 
             </div>
